630de8d6c22cc868ef96385d858912c279826e94
Lucas Herbert <lucasherbert.sousa@hotmail.com>
2022-11-14 12:52:04 -0300

    Tipo de Dados

---
+++ scripts.js
@@ -0,0 +1,60 @@
+// Comentário em linha
+
+/*
+    Comentário em bloco
+*/
+
+// Regras
+/*
+console.log('Olha eu no aspas simples!!')
+console.log('Olha eu no "aspas" simples!!')
+console.log(`Olha` `eu no aspas` `simples!!`)
+*/
+
+// Tipo números
+/*
+console.log(12) // Integer
+console.log(24.5) // Float
+console.log(12*24/4) //calc
+console.log(Infinity)
+console.log(50+(53*4.5))
+console.log(Math.round(50+(53*4.5)))
+*/
+
+// Tipo boleando
+/*
+console.log(false)
+console.log(true)
+console.log(24 > 32) // false
+console.log(42 > 32) // true
+console.log(42 = 32) // false
+*/
+
+// Tipo nulo e indefinido
+/*
+console.log(null)
+console.log(undefined)
+console.log(null = undefined) // false
+*/
+
+// Tipo objetos
+/*
+console.log({
+    marca: "FIAT",
+    largura: "550cm",
+    peso: 1,
+    velocidade: (90 * 200) * 2,
+    andar: function() {
+        console.log(`Sua velocidade é de 200`)
+    },
+    possuiAr: true
+})
+*/
+
+// Arrays
+console.log([
+    "Bolacha", // 0
+    "Coca-cola", // 1
+    "Verduras", // 2
+    "Agua" // 3
+])
